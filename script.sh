b8d4774d3b26924b6b1c1d731d2def286bb23e36
Gowtham-A <gowthamkarthic.1998@gmail.com>
2024-07-09 16:39:37 +0530

    First commond

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##############
+#
+#SHELL SCRIPT COMMOND                                                                                                                               #                                                                                                                                                   ##############                                                                                                                                      
+set -X # IS a debuging mood
+
+df -h #This is disk space cmmond
+
+free -g #this is memory aviable commond
+
+nproc #This is how may cpu in machine showing commond
+
+
+
